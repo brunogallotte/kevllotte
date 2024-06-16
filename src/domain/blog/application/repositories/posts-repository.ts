899820f1104c2a07b29059ab94ebbb2a4be6f867
Kevin <kevinsilvarj10@gmail.com>
2024-06-16 19:11:19 -0300

    feat: Creat Edit Post use case and tests for it

--- src/domain/blog/application/repositories/posts-repository.ts
+++ src/domain/blog/application/repositories/posts-repository.ts
@@ -3,4 +3,5 @@ import type { Post, Prisma } from '@prisma/client'
 export type PostsRepository = {
   findById(id: string): Promise<Post | null>
   create(data: Prisma.PostUncheckedCreateInput): Promise<Post>
+  update(data: Post): Promise<Post>
 }
