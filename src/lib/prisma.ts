e032836a77be502982c4f012225e1a64dd43622a
Kevin <kevinsilvarj10@gmail.com>
2024-07-26 07:27:59 -0300

    ci: Adjust database timeout

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
+import { env } from '@/env'
+
 declare global {
   // eslint-disable-next-line no-var
   var cachedPrisma: PrismaClient
@@ -8,7 +10,13 @@ declare global {
 let prismaInstance: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-  prismaInstance = new PrismaClient()
+  prismaInstance = new PrismaClient({
+    datasources: {
+      db: {
+        url: `${env.DATABASE_URL}?connect_timeout=20`,
+      },
+    },
+  })
 } else {
   if (!global.cachedPrisma) {
     global.cachedPrisma = new PrismaClient()
